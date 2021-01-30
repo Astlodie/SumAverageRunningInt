7a3542d336a87d0d5e06e6c705242e8f281d1ac6
Astlodie <71849560+Astlodie@users.noreply.github.com>
2021-01-30 13:06:31 -0500

    program to produce sum of 1,2,... to 100.

---
+++ SumAverageRunningInt/src/SumAverageRunningInt.java
@@ -0,0 +1,11 @@
+
+public class SumAverageRunningInt {
+public static void main(String[] args) {
+	int a=100, sum=0;
+	for (int i=1; i<=a; ++i)
+	{
+	sum +=i;
+	}
+System.out.println ("sum="+sum);
+}
+}
